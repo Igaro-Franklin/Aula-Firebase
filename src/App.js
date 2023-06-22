1530d80d5a3c54dac9b741c83d17e756c2bdf30b
Igaro-Franklin <igarofranklinr@gmail.com>
2023-06-22 20:59:37 -0300

    Enviando codigo do inicio de aula do firebase no react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { db } from './firebaseConnection';
+import { doc, setDoc, collection, addDoc, getDoc } from 'firebase/firestore'
+
+import './app.css'
+
+
+function handleAdd(){
+  alert('teste')
+}
 
 function App() {
+
+  const [titulo, setTitulo] = useState('');
+  const [autor, setAutor] = useState('');
+
+  async function handleAdd(){
+   // await setDoc(doc(db, "posts", "12345"), {
+   //   titulo: titulo,
+   //   autor: autor,
+   // })
+   // .then(() =>{
+   //  console.log("Dados registrado no banco")
+   // })
+   // .catch((error)=>{
+   //   console.log("gerou um erro " + error)
+   // })
+
+    await addDoc(collection(db, "posts"), {
+      titulo: titulo,
+      autor: autor,
+    })
+    .then(() =>{
+      console.log("Cadastrado com suceeso!")
+      setAutor('');
+      setTitulo('');
+    })
+    .catch((error) =>{
+      console.log("Erro "+ error)
+    })
+
+
+  }
+
+
+  async function buscarPost(){
+
+    const postRef = doc(db, "posts", "mTRt4mKLuqi5QBpYcA5X")
+
+    await getDoc(postRef)
+    .then((snapShot) =>{
+      setAutor(snapShot.data().autor)
+      setTitulo(snapShot.data().titulo)
+    })
+    .catch(() =>{
+      console.log("Erro ao buscar")
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <h1>React Js + firebase!!</h1>
+
+      <div className='container'>
+        <label>Título:</label>
+        <textarea 
+          placeholder='Digite o titulo' 
+          type='text'
+          value={titulo}
+          onChange={ (e) => setTitulo(e.target.value) }
         >
-          Learn React
-        </a>
-      </header>
+        </textarea>
+
+        <label>Autor:</label>
+        <input 
+          placeholder='Autor do post' 
+          type='text'
+          value={autor}
+          onChange={ (e) => setAutor(e.target.value) }
+        >
+        </input>
+
+        <button onClick={handleAdd}>Cadastrar:</button>
+        <button onClick={buscarPost}>Buscar post</button>
+      </div>
     </div>
   );
 }
---
+++ src/firebaseConnection.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAbYYGNrtDubIIu_QL7_ixAzj_B4SPeeqs",
+    authDomain: "cursosp-b0e82.firebaseapp.com",
+    projectId: "cursosp-b0e82",
+    storageBucket: "cursosp-b0e82.appspot.com",
+    messagingSenderId: "712267453122",
+    appId: "1:712267453122:web:fb103381dc2e9e66f4215b",
+    measurementId: "G-N9BL1G6SB4"
+  };
+
+  const firebaseApp = initializeApp(firebaseConfig);
+
+  const db = getFirestore(firebaseApp);
+
+  export { db };
